1e600762eab98f9aceffdeb68eb00a7814dba36a
DavidBarringer <davidlbarringer@aol.com>
2021-12-02 12:47:11 +0000

    Day 2 -comments +cursed

--- days/day02.lisp
+++ days/day02.lisp
@@ -2,28 +2,17 @@
 (setq test-sol-a 150)
 (setq test-sol-b 900)
 
-;; Input becomes list of instructions first map turns each instruction into it's own list
-;; second map takes the first character, parses the integer and makes the instruction a dotted pair e.g (#\f . 2)
 (defun parse-input (input-file)
 	(let ((input (mapcar (lambda (s) (split " " s)) (get-file-lines input-file))))
-		(mapcar (lambda (x) (cons (char (CAR x) 0) (parse-integer (CADR x)))) input)))
+		(mapcar (lambda (x) (cons (intern (string-upcase (CAR x))) (parse-integer (CADR x)))) input)))
 
 (defun part-a (parsed-input)
-	(move parsed-input 0 0))
+	(let ((forward 0) (up 0) (down 0))
+		(loop for x in parsed-input do (set (CAR x) (+ (eval (CAR x)) (CDR x))))
+		(* forward (- down up))))
 
 (defun part-b (parsed-input)
-	(move-b parsed-input 0 0 0))
-
-(defun move (instructions forward depth)
-	(COND
-		((null instructions) (* forward depth))
-		((eq (CAAR instructions) #\f) (move (CDR instructions) (+ forward (CDAR instructions)) depth))
-		((eq (CAAR instructions) #\u) (move (CDR instructions) forward (- depth (CDAR instructions))))
-		(t (move (CDR instructions) forward (+ depth (CDAR instructions))))))
-
-(defun move-b (instructions forward depth aim)
-	(COND
-		((null instructions) (* forward depth))
-		((eq (CAAR instructions) #\f) (move-b (CDR instructions) (+ forward (CDAR instructions)) (+ depth (* aim (CDAR instructions))) aim))
-		((eq (CAAR instructions) #\u) (move-b (CDR instructions) forward depth (- aim (CDAR instructions))))
-		(t (move-b (CDR instructions) forward depth (+ aim (CDAR instructions))))))
+	(let ((forward 0) (depth 0) (up 0) (down 0))
+		(loop for x in parsed-input do (set (CAR x) (+ (eval (CAR x)) (CDR x)))
+																	 (if (eq (CAR x) 'forward) (setf depth (+ depth (* (CDR x) (- down up))))))
+		(* forward depth)))
