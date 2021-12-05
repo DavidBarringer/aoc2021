e076149a2d3ea3c814ee5832be10562337ea1a03
DavidBarringer <davidlbarringer@aol.com>
2021-12-05 21:29:03 +0000

    Comments for day 5

--- days/day05.lisp
+++ days/day05.lisp
@@ -2,38 +2,39 @@
 (setq test-sol-a 5)
 (setq test-sol-b 12)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
+;; Split on -> then on , to get a list of co-ordinate pairs ((x1 y1) (x2 y2))
 (defun parse-input (input-file)
   (mapcar (lambda (s) (mapcar (lambda (c) (mapcar 'parse-integer (split "," c))) (split " -> " s))) (get-file-lines input-file)))
 
-;; Returns the solution for part a
+;; Make an array of the grid with each value initially set to zero. Go through each co-ordinate pair
+;; if either x1 & x2 or y1 & y2 are equal, go over the range of the other and increase the value
+;; at that position by one. Finally go over the array and count the number of elements greater than 1
 (defun part-a (parsed-input)
   (let* ((maxes (get-maxes parsed-input))
 	 (results (make-array (list (+ 1 (CDR maxes)) (+ 1 (CAR maxes))) :initial-element 0)))
     (loop for ((x1 y1) (x2 y2)) in parsed-input
 	  if (= x1 x2)
 	    do (loop for y from (min y1 y2) to (max y1 y2)
-		     do (incf (aref results y x1) 1))
+		     do (incf (aref results y x1)))
 	  else if (= y1 y2)
 		 do (loop for x from (min x1 x2) to (max x1 x2)
-			  do (incf (aref results y1 x) 1)))
-    (loop for x from 0 below (CAR (array-dimensions results))
-	  sum (loop for y from 0 below (CADR (array-dimensions results))
-		    if (>= (aref results x y) 2) count y))))
+			  do (incf (aref results y1 x))))
+    (loop for i from 0 below (array-total-size results)
+	  if (>= (row-major-aref results i) 2) count i)))
 
-;; Returns the solution for part b
+;; Has the same setup as part a, but includes cases for diagonals, there is a check if the gradient
+;; is negative so that the range of y co-ords can decrease as x increases
 (defun part-b (parsed-input)
   (let* ((maxes (get-maxes parsed-input))
 	 (results (make-array (list (+ 1 (CDR maxes)) (+ 1 (CAR maxes))) :initial-element 0)))
     (loop for ((x1 y1) (x2 y2)) in parsed-input
 	  if (= x1 x2)
 	    do (loop for y from (min y1 y2) to (max y1 y2)
-		     do (incf (aref results y x1) 1))
+		     do (incf (aref results y x1)))
 	  else if (= y1 y2)
 		 do (loop for x from (min x1 x2) to (max x1 x2)
-			  do (incf (aref results y1 x) 1))
-	  else if (OR (AND (> x2 x1) (> y1 y2)) (AND (> x1 x2) (> y2 y1)))
+			  do (incf (aref results y1 x)))
+	  else if (eq (> x2 x1) (> y1 y2))
 	    do (loop for x from (min x1 x2) to (max x1 x2)
 		     for y from (max y1 y2) downto (min y1 y2)
 		     do (incf (aref results y x)))
@@ -41,10 +42,10 @@
 	    do (loop for x from (min x1 x2) to (max x1 x2)
 		     for y from (min y1 y2) to (max y1 y2)
 		     do (incf (aref results y x))))
-    (loop for x from 0 below (CAR (array-dimensions results))
-	  sum (loop for y from 0 below (CADR (array-dimensions results))
-		    if (>= (aref results x y) 2) count y))))
+    (loop for i from 0 below (array-total-size results)
+	  if (>= (row-major-aref results i) 2) count i)))
 
+;; Gets the highest x co-ordinate and highest y co-ordinate and returns them as a dotted pair
 (defun get-maxes (input)
   (loop for ((x1 y1) (x2 y2)) in input
         maximize (max x1 x2) into x
