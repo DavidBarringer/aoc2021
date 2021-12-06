757e852fa254b760ddd5546870792f36b21bfd9b
DavidBarringer <davidlbarringer@aol.com>
2021-12-06 23:04:14 +0000

    Day 6 has some comments now

--- days/day06.lisp
+++ days/day06.lisp
@@ -2,16 +2,13 @@
 (setq test-sol-a 5934)
 (setq test-sol-b 26984457539)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
+;; Split on , then map to integers, simple.
 (defun parse-input (input-file)
   (setup (mapcar 'parse-integer (split "," (get-file-string input-file)))))
 
-;; Returns the solution for part a
 (defun part-a (parsed-input)
   (run-days parsed-input 80))
 
-;; Returns the solution for part b
 (defun part-b (parsed-input)
   (run-days parsed-input 256))
 
@@ -20,12 +17,14 @@
     (loop for n in input do (incf (nth n fish)))
     fish))
 
+;; For each day, move the list along 1 and add the removed number to the 7th position
 (defun run-days (fish days)
   (loop for i from 0 below days
 	for zeros = (pop fish)
 	do (incf (nth 6 fish) zeros))
   (loop for i from 0 to 8 for f in fish sum f))
 
+;; Turns a list into a circular list
 (defun circular (items)
   (setf (CDR (last items)) items)
   items)
