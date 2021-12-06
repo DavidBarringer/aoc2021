c19a8a10e35267301f298da9a5e851a7a7dc69bc
DavidBarringer <davidlbarringer@aol.com>
2021-12-06 13:46:08 +0000

    Day 6

--- days/day06.lisp
+++ days/day06.lisp
@@ -1,13 +1,31 @@
 ;; Put the given solutions for the examples here
-(defvar test-sol-a "No test solution")
-(defvar test-sol-b "No test solution")
+(setq test-sol-a 5934)
+(setq test-sol-b 26984457539)
 
 ;; Turn the input file into whatever form you will use for both parts
 ;; (get-file-lines) and (get-file-string) will be useful
-(defun parse-input (input-file))
+(defun parse-input (input-file)
+  (setup (mapcar 'parse-integer (split "," (get-file-string input-file)))))
 
 ;; Returns the solution for part a
-(defun part-a (parsed-input))
+(defun part-a (parsed-input)
+  (run-days parsed-input 80))
 
 ;; Returns the solution for part b
-(defun part-b (parsed-input))
+(defun part-b (parsed-input)
+  (run-days parsed-input 256))
+
+(defun setup (input)
+  (let ((fish (circular (loop for i from 0 to 8 collect 0))))
+    (loop for n in input do (incf (nth n fish)))
+    fish))
+
+(defun run-days (fish days)
+  (loop for i from 0 below days
+	for zeros = (pop fish)
+	do (incf (nth 6 fish) zeros))
+  (loop for i from 0 to 8 for f in fish sum f))
+
+(defun circular (items)
+  (setf (CDR (last items)) items)
+  items)
