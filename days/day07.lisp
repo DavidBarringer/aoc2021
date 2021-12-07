a7612c7ac08166f8fe10cef5487a458741bc669c
DavidBarringer <davidlbarringer@aol.com>
2021-12-07 13:16:44 +0000

    Day 7

--- days/day07.lisp
+++ days/day07.lisp
@@ -1,13 +1,18 @@
 ;; Put the given solutions for the examples here
-(defvar test-sol-a "No test solution")
-(defvar test-sol-b "No test solution")
+(setq test-sol-a 37)
+(setq test-sol-b 168)
 
 ;; Turn the input file into whatever form you will use for both parts
 ;; (get-file-lines) and (get-file-string) will be useful
-(defun parse-input (input-file))
+(defun parse-input (input-file)
+  (mapcar 'parse-integer (split "," (get-file-string input-file))))
 
 ;; Returns the solution for part a
-(defun part-a (parsed-input))
+(defun part-a (parsed-input)
+  (loop for n from 0 to (apply 'max parsed-input)
+	minimize (apply '+ (mapcar (lambda (x) (abs (- n x))) parsed-input))))
 
 ;; Returns the solution for part b
-(defun part-b (parsed-input))
+(defun part-b (parsed-input)
+  (loop for n from 0 to (apply 'max parsed-input)
+	minimize (apply '+ (mapcar (lambda (x) (/ (* (abs (- n x)) (+ 1 (abs (- n x)))) 2)) parsed-input))))
