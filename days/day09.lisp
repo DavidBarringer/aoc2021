fbd8722f037d1df8eb6cf2ae696101f9067512a4
DavidBarringer <davidlbarringer@aol.com>
2021-12-09 18:34:11 +0000

    Day 9 comments

--- days/day09.lisp
+++ days/day09.lisp
@@ -2,13 +2,15 @@
 (setq test-sol-a 15)
 (setq test-sol-b 1134)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
+;; Splits input on lines, then turns string into list of characters, then turns characters into digits
+;; Finally make a 2D array with the result
 (defun parse-input (input-file)
   (let ((input (mapcar (lambda (s) (mapcar 'digit-char-p (concatenate 'list s))) (get-file-lines input-file))))
     (make-array (list (length input) (length (CAR input))) :initial-contents input)))
 
-;; Returns the solution for part a
+;; Goes through each item in the array, and gets its neighbours. If a neighbour would be out-of-bounds,
+;; set it to the value of the current position + 1. Then check that the current position is less than all
+;; of it's neighbours and get that value + 1 if it is. Finally, sum all returned values.
 (defun part-a (parsed-input)
   (apply '+ (loop with width = (CAR (array-dimensions parsed-input))
 		  with height = (CADR (array-dimensions parsed-input))
@@ -21,7 +23,9 @@
 			      for below = (if (= height (+ 1 y)) (+ 1 current) (aref parsed-input x (+ 1 y)))
 			      if (AND (< current left) (< current right) (< current above) (< current below)) collect (+ 1 current)))))
 
-;; Returns the solution for part b
+;; Start by getting the co-ordinates of all tiles in basins. Then take the first one, get all
+;; of the neighbours in that basin and count how many there are. Repeat until all tiles are covered
+;; then get the biggest 3 basins and multiply their sizes
 (defvar cave-map)
 (defun part-b (parsed-input)
   (setq cave-map (loop with width = (CAR (array-dimensions parsed-input))
@@ -36,6 +40,10 @@
 		  '>)
 		 0 3)))
 
+;; Get the list of tile co-ordinates adjacent to current, if none of them are in the basin list,
+;; then the path is fully explored. Otherwise go through each adjacent tile in the basin and
+;; check the tiles adjacent to them. Continue until all adjacents have been explored and combine their
+;; co-ordinates with the current one's
 (defun count-adjacents (current)
   (let* ((x (CAR current))
 	 (y (CDR current))
