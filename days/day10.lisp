7a352e78fe24c7821a568d72efeae9739bdb561c
DavidBarringer <davidlbarringer@aol.com>
2021-12-10 22:58:07 +0000

    Day 10

--- days/day10.lisp
+++ days/day10.lisp
@@ -1,13 +1,57 @@
 ;; Put the given solutions for the examples here
-(defvar test-sol-a "No test solution")
-(defvar test-sol-b "No test solution")
+(setq test-sol-a 26397)
+(setq test-sol-b 288957)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
-(defun parse-input (input-file))
+(defun setup ()
+  (loop for bracket in '(#\) #\] #\} #\> #\< #\{ #\[ #\()
+	for matching in '(#\( #\[ #\{ #\< #\> #\{ #\] #\))
+	for score in '(3 57 1197 25137 4 3 2 1)
+	for name = (format nil "B-~A" bracket)
+	do (setf (symbol-value (intern name)) (make-bracket :matching matching :score score))))
 
-;; Returns the solution for part a
-(defun part-a (parsed-input))
+(defstruct bracket
+  score
+  matching)
 
-;; Returns the solution for part b
-(defun part-b (parsed-input))
+(setup)
+
+;; Turn each line into a list of characters
+(defun parse-input (input-file)
+  (mapcar (lambda (s) (concatenate 'list s)) (get-file-lines input-file)))
+
+;; For each line, keep a stack of opening bracket styles, if a closing bracket is seen and matches the opening one
+;; remove the opening from the stack, otherwise, there is an error, and return the score for that bracket
+(defun part-a (parsed-input)
+  (let ((opens '(#\( #\[ #\{ #\<)))
+    (apply '+ (remove nil
+		      (loop for input in parsed-input
+			    for stack = nil
+			    collect (loop for c in input
+					  for name = (format nil "B-~A" c)
+					  if (member c opens) do (push c stack)
+					    else if (equal (bracket-matching (symbol-value (intern name))) (CAR stack)) do (pop stack)
+						   else do (return (bracket-score (symbol-value (intern name))))))))))
+
+
+;; Similar to part a, but don't return errors, instead return the remaining stack when there are no errors, and get their score
+;; sum the scores of each opening bracket. Then find the middle value.
+(defun part-b (parsed-input)
+  (let* ((opens '(#\( #\[ #\{ #\<))
+	 (results (remove nil
+			  (loop for input in parsed-input
+				for stack = nil
+				collect (loop for c in input
+					      for name = (format nil "B-~A" c)
+					      if (member c opens) do (push c stack)
+						else if (equal (bracket-matching (symbol-value (intern name))) (CAR stack)) do (pop stack)
+						       else collect t into err
+					      finally (if (NOT (CAR err))
+							  (return (mapcar (lambda (s) (bracket-score (symbol-value (intern (format nil "B-~A" s))))) stack)))))))
+	 (scores (sort (loop for r in results
+			     collect (loop with score = 0
+					   for value in r
+					   do (setf score (+ value (* 5 score)))
+					   finally (return score))) '<)))
+    (nth (/ (- (length scores) 1) 2) scores)))
+
+
