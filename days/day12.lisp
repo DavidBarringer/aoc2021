9a33c58c54789c4b1b7edeac0df60c6b997a0873
DavidBarringer <davidlbarringer@aol.com>
2021-12-12 23:03:50 +0000

    Comments on day 12

--- days/day12.lisp
+++ days/day12.lisp
@@ -2,46 +2,52 @@
 (setq test-sol-a 19)
 (setq test-sol-b 103)
 
-(defstruct edge
+;; Make each cave a variable, with a list of caves it can travel to, and whether it is small
+(defstruct cave
   (reachables nil)
   small)
 
-;; Turn the input file into whatever form you will use for both parts
-;; (get-file-lines) and (get-file-string) will be useful
+;; Removes the - from the input, leaving pairs of caves
 (defun parse-input (input-file)
-  (mapcar (lambda (s) (split "-" s)) (get-file-lines input-file)))
+  (let ((input (mapcar (lambda (s) (split "-" s)) (get-file-lines input-file))))
+    (setup input)))
 
 ;; Returns the solution for part a
 (defun part-a (parsed-input)
-  (setup parsed-input)
   (length (move nil '|start| nil)))
 
 ;; Returns the solution for part b
 (defun part-b (parsed-input)
-  (setup parsed-input)
   (length (move-b nil '|start| nil nil)))
 
+;; Goes through the parsed input and makes a cave of each string
+;; Then re-goes through and adds to the list of reachables for each cave
 (defun setup (input)
   (loop for (e1 e2) in input
 	for e1-small = (NOT (string= e1 (string-upcase e1)))
 	for e2-small = (NOT (string= e2 (string-upcase e2)))
-	do (setf (symbol-value (intern e1)) (make-edge :small e1-small))
-	   (setf (symbol-value (intern e2)) (make-edge :small e2-small)))
+	do (setf (symbol-value (intern e1)) (make-cave :small e1-small))
+	   (setf (symbol-value (intern e2)) (make-cave :small e2-small)))
   (loop for (e1 e2) in input do
-    (if (NOT (string= "start" e1)) (push (intern e1) (edge-reachables (symbol-value (intern e2)))))
-    (if (NOT (string= "start" e2)) (push (intern e2) (edge-reachables (symbol-value (intern e1)))))))
-								     
-(defun move (visited edge path)
-  (if (edge-small (symbol-value edge)) (push edge visited))
-  (if (equal edge '|end|) (list (reverse (cons edge path)))
-      (loop for reachable in (edge-reachables (symbol-value edge))
-	    if (NOT (member reachable visited)) nconc (move visited reachable (cons edge path)))))
+    (if (NOT (string= "start" e1)) (push (intern e1) (cave-reachables (symbol-value (intern e2)))))
+    (if (NOT (string= "start" e2)) (push (intern e2) (cave-reachables (symbol-value (intern e1)))))))
 
-(defun move-b (visited edge path visit-2)
+;; If the cave is small, mark it as visited. If at the end of the path, return the path.
+;; Otherwise, go through each reachable cave from current and if it's not in the visited list,
+;; go to that cave and add the current one to the path
+(defun move (visited cave path)
+  (if (cave-small (symbol-value cave)) (push cave visited))
+  (if (equal cave '|end|) (list (reverse (cons cave path)))
+      (loop for reachable in (cave-reachables (symbol-value cave))
+	    if (NOT (member reachable visited)) nconc (move visited reachable (cons cave path)))))
+
+;; Similar to the previous, but if any small cave has been visited twice and the reachable has been
+;; visited before then the reachable is discarded.
+(defun move-b (visited cave path visit-2)
   (let ((temp-visit-2 nil))
     (if (OR visit-2
-	 (AND (edge-small (symbol-value edge)) (member edge visited) (NOT (equal edge '|start|)))) (setf temp-visit-2 t))
-    (if (edge-small (symbol-value edge)) (push edge visited))
-    (if (equal edge '|end|) (list (reverse (cons edge path)))
-	(loop for reachable in (edge-reachables (symbol-value edge))
-	      if (NOT (AND temp-visit-2 (member reachable visited))) nconc (move-b visited reachable (cons edge path) temp-visit-2)))))
+	 (AND (cave-small (symbol-value cave)) (member cave visited) (NOT (equal cave '|start|)))) (setf temp-visit-2 t))
+    (if (cave-small (symbol-value cave)) (push cave visited))
+    (if (equal cave '|end|) (list (reverse (cons cave path)))
+	(loop for reachable in (cave-reachables (symbol-value cave))
+	      if (NOT (AND temp-visit-2 (member reachable visited))) nconc (move-b visited reachable (cons cave path) temp-visit-2)))))
